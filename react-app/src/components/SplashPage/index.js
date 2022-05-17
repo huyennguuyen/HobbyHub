b709019f167337348d0c3656e044c050d1078ca7
Huyen Nguyen <hn078722@gmail.com>
2022-05-17 03:00:49 +0000

    editing backend of group routes but changed it back

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -9,7 +9,7 @@ export default function SplashPage () {
             <div className="background">
                 <div className="splash-text">
                     <h1 className="splash-header">Join Hobby Hub to make new connections and expand your knowledge.</h1>
-                    <p className="splash-paragraph">Whatever you’re looking to do this year, Meetup can help. For 20 years, people have turned to Meetup to meet people, make friends, find support, grow a business, and explore their interests. Thousands of events are happening every day—join the fun.</p>
+                    <p className="splash-paragraph">Whatever you’re looking to do this year, Hobby Hub can help. People have turned to Meetup to meet people, make friends, find support, grow a business, and explore their interests. Thousands of events are happening every day—join the fun.</p>
                 </div>
                 <img src={hobby} className="hobby"></img>
             </div>
