ecaf77f8a0cfcefc332b100e27b5582ad465cb67
Huyen Nguyen <hn078722@gmail.com>
2022-05-18 00:05:02 +0000

    email validators

--- react-app/src/components/LoginForm/LoginForm.js
+++ react-app/src/components/LoginForm/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, Redirect, useHistory } from 'react-router-dom';
 import { login } from '../../store/session';
@@ -13,10 +13,23 @@ const LoginForm = () => {
   const [password, setPassword] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
-  const [hasClicked, sethasClicked] = useState(false)
+  const [hasSubmitted, setHasSubmitted] = useState(false)
+
+//   useEffect(() => {
+//     let errors = [];
+//     if(!email.length) errors.push("Please enter a email.")
+//     if(!password.length) errors.push("Please enter a password.")
+//     // if(!image) errors.push("Please upload an image")
+//     setErrors(errors)
+// }, [email, password])
 
   const onLogin = async (e) => {
     e.preventDefault();
+
+    // setHasSubmitted(true)
+
+    // if (errors.length > 0) return;
+    
     const data = await dispatch(login(email, password));
     if (data) {
       setErrors(data);
@@ -33,13 +46,6 @@ const LoginForm = () => {
     setPassword(e.target.value);
   };
 
-  const submitting = (e) => {
-    e.preventDefault()
-
-    hasClicked(false)
-
-
-  }
 
   if (user) {
     return <Redirect to='/home' />;
@@ -54,7 +60,7 @@ const LoginForm = () => {
           <img src={icon} className="icon"></img>
           <h2 className="login header">Log in</h2>
           <div>
-            {errors.map((error, ind) => (
+            { errors.map((error, ind) => (
               <div key={ind}>{error}</div>
             ))}
           </div>
