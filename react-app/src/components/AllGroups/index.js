8a1fa5531898a7a2a57d4ca140a1ebdb2c6e8c20
Huyen Nguyen <hn078722@gmail.com>
2022-05-20 18:11:16 +0000

    aligned everything for the my groups box with menu

--- react-app/src/components/AllGroups/index.js
+++ react-app/src/components/AllGroups/index.js
@@ -5,6 +5,7 @@ import * as groupActions from "../../store/group"
 import EditGroup from "../EditGroupForm/EditGroupForm";
 import DeleteGroup from "../DeleteGroup";
 import { Modal } from "../context/Modal";
+import {FiMoreHorizontal} from "react-icons/fi"
 // import AllGroups from "../AllGroups";
 import "./AllGroups.css"
 
@@ -21,25 +22,35 @@ export default function AllGroups ({group}){
     return (
         <>
         <div className="my-groups-box" key={group.id}>
-            <NavLink to={`/groups/${group?.id}`}>
-            <img src={group.backgroundImage} className="image"></img>
-            </NavLink>
-            <div className="my-groups-description">
-                <h2 className="group-name">{group?.name}</h2>
-                <p className="group-description">{group?.description}</p>
-                <div className="my-group-buttons-box"> 
-                    <button onClick={e => setShowEditGroup(true)} className="my-group-button">Edit Group</button>
-                    {showEditGroup && (
-                    <Modal onClose={() => setShowEditGroup(false)}> 
-                        <EditGroup closeModal={() => setShowEditGroup(false)}  group={group}/>
-                    </Modal>
-                    )}
-                    <button onClick={ () => setShowDeleteGroup(true)} className="my-group-button delete-my-group">Delete</button>
-                    {showDeleteGroup && (
-                    <Modal onClose={() => setShowDeleteGroup(false)}>
-                        <DeleteGroup closeModal={() => setShowDeleteGroup(false)} group={group}/>
-                    </Modal>
-                    )} 
+            <div className="row-menu">
+                <NavLink to={`/groups/${group?.id}`}>
+                <img src={group.backgroundImage} className="image"></img>
+                </NavLink>
+                <div className="my-groups-description">
+                    <h2 className="group-name">{group?.name}</h2>
+                    <p className="group-description">{group?.description}</p>
+                </div>
+            </div>
+            <div className="menu-box">
+                <div className="dropdown">
+                    <button class="link"><FiMoreHorizontal /></button>
+                    <div className="dropdown-menu">
+                        Dropdown Content
+                        <div className="my-group-buttons-box"> 
+                            <button onClick={e => setShowEditGroup(true)} className="my-group-button">Edit Group</button>
+                            {showEditGroup && (
+                            <Modal onClose={() => setShowEditGroup(false)}> 
+                                <EditGroup closeModal={() => setShowEditGroup(false)}  group={group}/>
+                            </Modal>
+                            )}
+                            <button onClick={ () => setShowDeleteGroup(true)} className="my-group-button delete-my-group">Delete</button>
+                            {showDeleteGroup && (
+                            <Modal onClose={() => setShowDeleteGroup(false)}>
+                                <DeleteGroup closeModal={() => setShowDeleteGroup(false)} group={group}/>
+                            </Modal>
+                            )} 
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
