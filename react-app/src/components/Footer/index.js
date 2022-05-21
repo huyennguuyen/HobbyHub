a0a24662169046767b51d6259ecfeb429b4e3dd8
Huyen Nguyen <hn078722@gmail.com>
2022-05-21 05:46:35 +0000

    footer done

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,10 +6,20 @@ export default function Footer () {
 
     return (
         <div className="developer-info">
-            <p className="information">
-                <a className="developer-link" href="https://github.com/huyennguuyen">GitHub <AiFillGithub /></a>
-                <a className="linkedin" href="https://www.linkedin.com/in/huyen-nguyen-2804b523b/">LinkedIn <AiFillLinkedin/></a>
-            </p>
+            <div className="about">
+                <div className="information">
+                    <div className="inside-information">
+                        <p className="more-info">
+                            @2022 Hobby Hub
+                        </p>
+                        <p className="more-info">Created By: Huyen Nguyen </p>
+                    </div>
+                </div>
+                <div className="links">
+                    <a className="developer-link" href="https://github.com/huyennguuyen">GitHub <AiFillGithub /></a>
+                    <a className="developer-link" href="https://www.linkedin.com/in/huyen-nguyen-2804b523b/">LinkedIn <AiFillLinkedin/></a>
+                </div>
+            </div>
         </div>
     )
 
