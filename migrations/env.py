719773cbc68511f138bc484096b5fa3657978b2a
Huyen Nguyen <hn078722@gmail.com>
2022-11-25 02:36:52 +0000

    adding to the env file in migrations

--- migrations/env.py
+++ migrations/env.py
@@ -86,7 +86,12 @@ def run_migrations_online():
             **current_app.extensions['migrate'].configure_args
         )
 
+        if environment == "production":
+            connection.execute(f"CREATE SCHEMA IF NOT EXISTS {SCHEMA}")
+
         with context.begin_transaction():
+            if environment == "production":
+                context.execute(f"SET search_path TO {SCHEMA}")
             context.run_migrations()
 
 
@@ -94,3 +99,7 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get('SCHEMA')
