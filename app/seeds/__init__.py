47cbfae04e46daf897ea000ca328403c83b439fb
Huyen Nguyen <hn078722@gmail.com>
2022-11-30 11:06:32 +0000

    seeds in the wrong place for seeds/init

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,11 +5,20 @@ from .users import seed_users, undo_users
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
-
-# Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
     seed_users()
+
+
+# Creates the `flask seed all` command
+# @seed_commands.command('all')
+# def seed():
+#     seed_users()
     # Add other seed functions here
 
 
