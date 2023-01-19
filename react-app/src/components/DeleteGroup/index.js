5893599b4703f7c43032c27474111480e1a9b5d8
Huyen Nguyen <hn078722@gmail.com>
2023-01-19 03:34:13 +0000

    adding an x to the edit/delete form

--- react-app/src/components/DeleteGroup/index.js
+++ react-app/src/components/DeleteGroup/index.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { removeGroup } from '../../store/group';
 import "./DeleteGroup.css"
 
-function DeleteGroup({ closeModal, group }) {
+function DeleteGroup({ close, group }) {
     const dispatch = useDispatch();
     const [errors, setErrors] = useState([]);
 
@@ -15,15 +15,17 @@ function DeleteGroup({ closeModal, group }) {
             const data = await res.json();
             if (data && data.errors) setErrors(data.errors);
         });
-        closeModal();
+        close();
     }
 
     const handleCancelClick = (e) => {
         e.preventDefault()
-        closeModal();
+        close();
     };
 
     return (
+        <>
+        <img src="https://res.cloudinary.com/daeopbcax/image/upload/v1667352522/Huyen/cancel_ywptyy.png" className="cancel-button pointer" onClick={handleCancelClick}></img>
         <div className="delete-box">
             <form className="delete-form" onSubmit={e => handleSubmit(e, group)}>
                 <h3 className="delete-text">Are you sure you want to delete your group?</h3>
@@ -36,6 +38,7 @@ function DeleteGroup({ closeModal, group }) {
                 </div>
             </form>
         </div>
+        </>
     )
 }
 
