f0e669b6fbb6b8633538cc2ec7115315996aff5d
Huyen Nguyen <hn078722@gmail.com>
2023-01-31 14:43:06 +0000

    fixed the memory leak and the board telling to create a group

--- react-app/src/components/MyGroups/index.js
+++ react-app/src/components/MyGroups/index.js
@@ -14,7 +14,8 @@ function MyGroups() {
     const sessionUser = useSelector(state => state.session.user);
     const groupState = useSelector(state => state.groups)
     const groups = Object.values(groupState)
-    const [state, setState] = useState()
+    const [myCreatedGroups, setmyCreatedGroups] = useState()
+    const [loading, setLoading] = useState()
     // console.log("this is groups state --------", groupState)
     // const [showEditGroup, setShowEditGroup] = useState(false)
     
@@ -23,16 +24,37 @@ function MyGroups() {
         
     //     await dispatch(groupActions.clearingGroup())
         
-    // }, )
+    // }, [])
     // console.log("THIS IS GROUPS------------------", groups)
+
+    const filterGroups = groups.filter(group => group?.ownerId === sessionUser?.id)
+ 
     
     useEffect(async() => {
         if (!sessionUser) history.push('/')
         // if (sessionUser) await dispatch(groupActions.clearingGroup())
         if (sessionUser) await dispatch(groupActions.getGroupsHome())
+
     }, [sessionUser, dispatch])
 
+  
+    
+
     console.log("THIS IS GROUPS Second ------------------", groups)
+
+    const loadingMore = () => {
+
+        if (filterGroups) {
+            return setLoading(false)
+        } else {
+            return setLoading(true)
+        }
+    }
+
+    //  useEffect(() => {
+    //    setmyCreatedGroups(filterGroups)
+
+    // }, [filterGroups])
 
 
     // useEffect(async() => {
@@ -56,23 +78,25 @@ function MyGroups() {
 
     // remember that groups.length is different from just groups and 
     // you removed the console.log from the navbar.js
+
+  
+
  
     return (
         <div className="my-groups-container">
             <div className="my-groups-center">
                 <h1 className="my-groups-header"> My Created Groups </h1>
-                {groups && (groups
-                    ?.filter(group => group?.id === sessionUser?.id)
-                     .map(group => 
+                { filterGroups && (filterGroups
+                    .map(group => 
                     <AllGroups key={group.id} group={group}/>) )}
-                    {groups?.length === 0 && (
+                    {filterGroups?.length === 0 && (
                     <div className="no-groups"> 
                         <h2 style={{fontSize: "35px"}}>Thank you for signing up!</h2>
                         <h3 style={{fontSize: "23px", marginBottom: "30px"}}>Get started by creating a group.</h3>
                         <NavLink to="/groups/new">
                         <button className="pointer"style={{color: "white", border: "none", backgroundColor: "#E86652", height: "28px", fontFamily: "PT Serif", fontSize: "15px", borderRadius: "3px" }}>Create a group</button>
                         </NavLink >
-                    </div>   
+                    </div>
                 )}
             </div>
         </div>
